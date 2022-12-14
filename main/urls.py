b36db5f1c0f406906cb9fd8c808fef5df3ed18a9
JaloliddinRuzikulov <jaloliddin1ruzikulov@gmail.com>
2022-12-15 00:47:19 +0500

    template fixed

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, ProductsPageView, ContactPageView
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductsPageView.as_view(), name='products'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+
+class ProductsPageView(TemplateView):
+    template_name = 'product.html'
